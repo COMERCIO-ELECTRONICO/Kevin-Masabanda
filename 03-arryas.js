bc5b43c624eaa4df9d026c400b0fd092df822768
KEVIN <kmasabandacuenca@gmail.com>
2020-05-07 11:59:35 -0500

    subir

--- 03-arryas.js
+++ 03-arryas.js
@@ -1,28 +1,103 @@
-var arregloNumero = [1,2,3,4]
-console.log(arregloNumero[2])
-//slice y splice
-//var arregloSlice = arregloNumero.slice(0,1)
+var  arregloNumero  =  [ 1 ,  2 ,  3 ,  4 ]
+// console.log (arregloNumero)
+// indexOf, encontrando el indice del elemento
+// console.log (arregloNumero.indexOf (0))
+// agregar elementos, Push
+//console.log(arregloNumero.push(10))
+// console.log (arregloNumero)
+// acceder elementos
+// console.log (arregloNumero [2])
+// rebanada y empalme
+
+// slice => dicidir el vector original;
+var  arregloSlice  =  arregloNumero . rebanada ( 0 ,  1 )
 //console.log(arregloSlice);
 //console.log(arregloNumero);
-//Pop: elimina ultimo elemento
-///console.log(arregloNumero.pop())
+// splice => dicidir el vector original;
+// var arregloSplice = arregloNumero.splice (2, 1)
+//console.log(arregloSplice);
+//console.log(arregloNumero);
+
+// Pop => elimina ultimo elemento
+
+//console.log (arregloNumero.pop ())
 //console.log(arregloNumero)
 
-var arreglo2 = ['kevin', 1, true { numero: 1}];
-/*arreglo2
-.forEach(
-    function(item,indice,arreglo){
-        console.log('item: '+ item);
-        console.log('indice: '+ indice);
-        console.log('arreglo '+ arreglo);
-}
-)*/
-//map: returno arreglo, mutando o modificando el arreglo 
-var arregloMutado = arreglo2
-.map(
-    (item,indice,arreglo)=>{
-console.log(item)
-return item+1;
+var  arreglo2  =  [ 1 ,  2 ,  3 ,  4 ] ;
+/ * arreglo2
+    .para cada(
+        función (ítem, índice, arreglo) {
+            console.log ('item:' + item);
+            console.log ('indice:' + indice);
+            console.log ('arreglo:' + arreglo);
+        }
+    ) * /
+
+// map => returnno arreglo, muta al arreglo o lo modifica
+/ * var arregloMutado = arreglo2
+    .mapa(
+        (item, indice, arreglo) => {
+            console.log (elemento)
+            if (elemento === 2) {
+                devolver artículo + 2;
+            }
+            devolver artículo + 1;
+        }
+    )
+console.log (arregloMutado); * /
+
+// filter => retorna arreglor dependien de la condicion
+
+/ * var arregloFilter = arreglo2
+    .filtrar(
+        (item, valor, arreglo) => {
+            devolver artículo <3
+        }
+    )
+console.log (arregloFilter); * /
+
+// find => retorn un valor
+/ * var valorFind = arreglo2
+    .encontrar(
+        (item, indice, arreglo) => {
+            artículo devuelto === 1
+        }
+    )
+    console.log (valorFind); * /
+
+// todos y algunos
+// cada verificación si todo los valores corresponden a una condición 
+// verdadero Falso
+// algunos verifica si almenos uno requerido una condicion 
+
+
+var  operadoresMapyFilter  =  arreglo2
+    . mapa (
+        ( artículo )  =>  {
+            devolver el  artículo * 2 ;
+        }
+    ) . filtro (
+        ( artículo )  =>  {
+             artículo  devuelto ==  4
+        }
+    )
+
+arreglo  uno ,  multiplique * 2 ,  y  me  filtrar  los  pares ,  usar  map  y  filter
+arreglo  dos ,  filtro  los  los  impares  ,  sumar  +  2
+
+{
+    arreglos : {
+        uno : [ 2 ,  4 ,  5 ,  8 ,  6 ,  9 ,  10 ] ,
+        dos : [
+            { valor : 1 } ,
+            { valor : 4 } ,
+            { valor : 2 } ,
+            { valor : 24 } ,
+            { valor : 100 } ,
+            { valor : 23 } ,
+            { valor : 45 } ,
+            { valor : 23 }
+        ] ,
     }
-)
-console.log(arregloMutado);
+
+}
