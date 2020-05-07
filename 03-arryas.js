eb45677a8db3aea46d228dfaf7a5b0858d781a32
KEVIN <kmasabandacuenca@gmail.com>
2020-05-07 18:52:21 -0500

    listo

--- 03-arryas.js
+++ 03-arryas.js
@@ -1,103 +1,104 @@
-var  arregloNumero  =  [ 1 ,  2 ,  3 ,  4 ]
-// console.log (arregloNumero)
-// indexOf, encontrando el indice del elemento
-// console.log (arregloNumero.indexOf (0))
-// agregar elementos, Push
+var arregloNumero = [1, 2, 3, 4]
+// console.log(arregloNumero)
+// indexOf, encontra el indice del elemento
+// console.log(arregloNumero.indexOf(0))
+// añadir elementos, Push
 //console.log(arregloNumero.push(10))
-// console.log (arregloNumero)
+// console.log(arregloNumero)
 // acceder elementos
-// console.log (arregloNumero [2])
-// rebanada y empalme
+// console.log(arregloNumero[2])
+// slice y splice
 
 // slice => dicidir el vector original;
-var  arregloSlice  =  arregloNumero . rebanada ( 0 ,  1 )
+var arregloSlice = arregloNumero.slice(0, 1)
 //console.log(arregloSlice);
 //console.log(arregloNumero);
 // splice => dicidir el vector original;
-// var arregloSplice = arregloNumero.splice (2, 1)
+//var arregloSplice = arregloNumero.splice(2, 1)
 //console.log(arregloSplice);
 //console.log(arregloNumero);
 
 // Pop => elimina ultimo elemento
 
-//console.log (arregloNumero.pop ())
+//console.log(arregloNumero.pop())
 //console.log(arregloNumero)
 
-var  arreglo2  =  [ 1 ,  2 ,  3 ,  4 ] ;
-/ * arreglo2
-    .para cada(
-        función (ítem, índice, arreglo) {
-            console.log ('item:' + item);
-            console.log ('indice:' + indice);
-            console.log ('arreglo:' + arreglo);
+var arreglo2 = [1, 2, 3, 4];
+/*arreglo2
+    .forEach(
+        function (item, indice, arreglo) {
+            console.log('item: ' + item);
+            console.log('indice: ' + indice);
+            console.log('arreglo: ' + arreglo);
         }
-    ) * /
+    )*/
 
-// map => returnno arreglo, muta al arreglo o lo modifica
-/ * var arregloMutado = arreglo2
-    .mapa(
+// map => returno arreglo, muta al arreglo o lo modifica
+var arregloMutado = arreglo2
+    .map(
         (item, indice, arreglo) => {
-            console.log (elemento)
-            if (elemento === 2) {
-                devolver artículo + 2;
+            console.log(item)
+            if (item === 2) {
+                return item + 2;
             }
-            devolver artículo + 1;
+            return item + 1;
         }
     )
-console.log (arregloMutado); * /
+console.log(arregloMutado);
 
 // filter => retorna arreglor dependien de la condicion
 
-/ * var arregloFilter = arreglo2
-    .filtrar(
+/*var arregloFilter = arreglo2
+    .filter(
         (item, valor, arreglo) => {
-            devolver artículo <3
+            return item < 3
         }
     )
-console.log (arregloFilter); * /
+console.log(arregloFilter);*/
 
 // find => retorn un valor
-/ * var valorFind = arreglo2
-    .encontrar(
+/*var valorFind = arreglo2
+    .find(
         (item, indice, arreglo) => {
-            artículo devuelto === 1
+            return item === 1
         }
     )
-    console.log (valorFind); * /
+    console.log(valorFind);*/
 
-// todos y algunos
-// cada verificación si todo los valores corresponden a una condición 
-// verdadero Falso
-// algunos verifica si almenos uno requerido una condicion 
+// every y some
+// every verifica si todo los valores cumplen una condicion 
+// true, false
+// some verifica si almenos uno cumplen una condicion 
 
 
-var  operadoresMapyFilter  =  arreglo2
-    . mapa (
-        ( artículo )  =>  {
-            devolver el  artículo * 2 ;
+/*var operadoresMapyFilter = arreglo2
+    .map(
+        (item) => {
+            return item * 2;
         }
-    ) . filtro (
-        ( artículo )  =>  {
-             artículo  devuelto ==  4
+    ).filter(
+        (item) => {
+            return item == 4
         }
-    )
+    ) 
 
-arreglo  uno ,  multiplique * 2 ,  y  me  filtrar  los  pares ,  usar  map  y  filter
-arreglo  dos ,  filtro  los  los  impares  ,  sumar  +  2
+/*
+    arreglo uno, multiplique * 2, y me filtrar los pares, usar map y filter
+arreglo dos, filter los los impares , sumar + 2
 
 {
-    arreglos : {
-        uno : [ 2 ,  4 ,  5 ,  8 ,  6 ,  9 ,  10 ] ,
-        dos : [
-            { valor : 1 } ,
-            { valor : 4 } ,
-            { valor : 2 } ,
-            { valor : 24 } ,
-            { valor : 100 } ,
-            { valor : 23 } ,
-            { valor : 45 } ,
-            { valor : 23 }
-        ] ,
+    arreglos: {
+        uno: [2, 4, 5, 8, 6, 9, 10],
+        dos: [
+            {valor: 1},
+            {valor: 4},
+            {valor: 2},
+            {valor: 24},
+            {valor: 100},
+            {valor: 23},
+            {valor: 45},
+            {valor: 23}
+        ],
     }
 
-}
+} */
--- 04-deber.js
+++ 04-deber.js
@@ -1,60 +1,47 @@
-
-/*JSON
-{
+//Arreglo uno, multiplique * 2, y me filtrar los pares, usar map y filter
+//Arreglo dos, filter los los impares , sumar + 2
+var json = {
     arreglos: {
-        uno: [2, 4, 5, 8, 6, 9, 10],
-        dos: [
-            {valor: 1},
-            {valor: 4},
-            {valor: 2},
-            {valor: 24},
-            {valor: 100},
-            {valor: 23},
-            {valor: 45},
-            {valor: 23}
-        ],
-    }
-    */
-var json ={
-    multiplicar: (a,b)=>{
-return a * b;
-    }
-}
-console.log(json.multiplicar(2,2));
-var json ={
-    multiplicar: (a,b)=>{
-return a * b;
-    }
-}
-console.log(json.multiplicar(2,4));
-var json ={
-    multiplicar: (a,b)=>{
-return a * b;
-    }
-}
-console.log(json.multiplicar(2,5));
-var json ={
-    multiplicar: (a,b)=>{
-return a * b;
-    }
-}
-console.log(json.multiplicar(2,8));
-var json ={
-    multiplicar: (a,b)=>{
-return a * b;
-    }
-}
-console.log(json.multiplicar(2,6));
-var json ={
-    multiplicar: (a,b)=>{
-return a * b;
-    }
-}
-console.log(json.multiplicar(2,9));
-var json ={
-    multiplicar: (a,b)=>{
-return a * b;
-    }
-}
-console.log(json.multiplicar(2,10));
-
+      primero: [2, 4, 5, 8, 6, 9, 10],
+      segundo: [
+        { valor: 1 },
+        { valor: 4 },
+        { valor: 2 },
+        { valor: 24 },
+        { valor: 100 },
+        { valor: 23 },
+        { valor: 45 },
+        { valor: 23 },
+      ],
+    },
+  };
+  //MULTIPLICAR X2
+  var PRIMERO1 = json.arreglos.primero
+  .map((item, index, arreglos) => {
+    return item *2
+  });
+  console.log("MULTIPLICACION X2")
+  console.log(PRIMERO1)
+  //FILTRO NUMEROS PARES
+  var SEGUNDO1 = json.arreglos.primero
+  .filter((item, valor, arreglos) => {
+    return item===2|item=== 4|item===6|item===8|item===10 
+  });
+  console.log("PARES")
+  console.log(SEGUNDO1)
+  
+  //ACTIVIDAD II
+  //FILTRO NUMEROS IMPARES
+  var SEGUNDO1 = json.arreglos.segundo
+  .filter((item, valor, arreglos) => {
+      return item.valor===1|item.valor===23|item.valor===45;
+    });
+    console.log("IMPARES")
+    console.log(SEGUNDO1) 
+  //SUMAR +2
+  var SEGUNDO2 = json.arreglos.segundo
+  .map((item, index, arreglos) => {
+    return item.valor+2
+  });
+  console.log("SUMA +2");
+  console.log(SEGUNDO2);
