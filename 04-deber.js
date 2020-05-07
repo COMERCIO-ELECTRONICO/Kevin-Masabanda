dee4e359371f30c367c54ff1eda86a78230364e1
KEVIN <kmasabandacuenca@gmail.com>
2020-05-07 15:56:36 -0500

    lsf

---
+++ 04-deber.js
@@ -0,0 +1,60 @@
+
+/*JSON
+{
+    arreglos: {
+        uno: [2, 4, 5, 8, 6, 9, 10],
+        dos: [
+            {valor: 1},
+            {valor: 4},
+            {valor: 2},
+            {valor: 24},
+            {valor: 100},
+            {valor: 23},
+            {valor: 45},
+            {valor: 23}
+        ],
+    }
+    */
+var json ={
+    multiplicar: (a,b)=>{
+return a * b;
+    }
+}
+console.log(json.multiplicar(2,2));
+var json ={
+    multiplicar: (a,b)=>{
+return a * b;
+    }
+}
+console.log(json.multiplicar(2,4));
+var json ={
+    multiplicar: (a,b)=>{
+return a * b;
+    }
+}
+console.log(json.multiplicar(2,5));
+var json ={
+    multiplicar: (a,b)=>{
+return a * b;
+    }
+}
+console.log(json.multiplicar(2,8));
+var json ={
+    multiplicar: (a,b)=>{
+return a * b;
+    }
+}
+console.log(json.multiplicar(2,6));
+var json ={
+    multiplicar: (a,b)=>{
+return a * b;
+    }
+}
+console.log(json.multiplicar(2,9));
+var json ={
+    multiplicar: (a,b)=>{
+return a * b;
+    }
+}
+console.log(json.multiplicar(2,10));
+
