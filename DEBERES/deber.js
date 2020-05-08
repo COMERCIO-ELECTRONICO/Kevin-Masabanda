18077e875f5851160391d0b781b2fb4df80ab574
KEVIN <kmasabandacuenca@gmail.com>
2020-05-07 19:07:13 -0500

    listo

---
+++ DEBERES/deber.js
@@ -0,0 +1,45 @@
+var json = {
+    arreglos: {
+      primero: [2, 4, 5, 8, 6, 9, 10],
+      segundo: [
+        { valor: 1 },
+        { valor: 4 },
+        { valor: 2 },
+        { valor: 24 },
+        { valor: 100 },
+        { valor: 23 },
+        { valor: 45 },
+        { valor: 23 },
+      ],
+    },
+  };
+  //MULTIPLICAR X2
+  var PRIMERO1 = json.arreglos.primero
+  .map((item, index, arreglos) => {
+    return item *2
+  });
+  console.log("MULTIPLICACION X2")
+  console.log(PRIMERO1)
+  //FILTRO NUMEROS PARES
+  var SEGUNDO1 = json.arreglos.primero
+  .filter((item, valor, arreglos) => {
+    return item===2|item=== 4|item===6|item===8|item===10 
+  });
+  console.log("PARES")
+  console.log(SEGUNDO1)
+  
+  //ACTIVIDAD II
+  //FILTRO NUMEROS IMPARES
+  var SEGUNDO1 = json.arreglos.segundo
+  .filter((item, valor, arreglos) => {
+      return item.valor===1|item.valor===23|item.valor===45;
+    });
+    console.log("IMPARES")
+    console.log(SEGUNDO1) 
+  //SUMAR +2
+  var SEGUNDO2 = json.arreglos.segundo
+  .map((item, index, arreglos) => {
+    return item.valor+2
+  });
+  console.log("SUMA +2");
+  console.log(SEGUNDO2);
