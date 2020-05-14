d294ac550af02aa9e53883b282a09441af497348
KEVIN <kmasabandacuenca@gmail.com>
2020-05-14 12:34:43 -0500

    padre-hijo

--- prueba/src/app/app.component.ts
+++ prueba/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba';
+
+  titulo = 'soy tu padre';
+  nombreBoton = 'alertar';
+  imagen = 'https://as.com/futbol/imagenes/2020/01/27/album/1580109965_448579_1580110002_album_grande.jpg';
 }
--- prueba/src/app/mi-primer-componente/mi-primer-componente.component.ts
+++ prueba/src/app/mi-primer-componente/mi-primer-componente.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input} from '@angular/core';
+//@componentDecorador() decorador 
 @Component({
   selector: 'app-mi-primer-componente',
   templateUrl: './mi-primer-componente.component.html',
@@ -7,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiPrimerComponenteComponent implements OnInit {
 
+  @Input() titulo: string;
+  @Input() nombreBoton: string;
+  @Input() imagenPath: string;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-  
+
 SALUDAR(){
   alert('holaaaaaa');
 }
