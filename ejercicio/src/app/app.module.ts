7c0b5224421902db4d0230aca71323ba5b9bb5c7
KEVIN <kmasabandacuenca@gmail.com>
2020-05-25 15:12:59 -0500

    rutasPerfil

--- ejercicio/src/app/app.module.ts
+++ ejercicio/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { AppRoutes } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -7,23 +6,33 @@ import { HomeComponent } from './home/home.component';
 import { NoEncontradoComponent } from './no-encontrado/no-encontrado.component';
 import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 import { LoginComponent } from './login/login.component';
+import { AppRoutes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {ButtonModule} from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import { PerfilUsuarioComponent } from './perfil-usuario/perfil-usuario.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NoEncontradoComponent,
     IniciarSesionComponent,
-    LoginComponent
+    LoginComponent,
+    PerfilUsuarioComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutes,
     BrowserAnimationsModule,
-    ButtonModule
+    ButtonModule,
+    FormsModule,
+    MatInputModule,
+    AutoCompleteModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
